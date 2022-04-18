0b4013656584fa8eee795410e2d213f79424d55f
Jairo Castaneda <jairo.castaneda@uptc.edu.co>
2022-04-18 12:54:47 -0500

    FrontEnd -> se crean los componentes Dashboard

--- FrontEnd/src/app/components/inicio/register/register.component.ts
+++ FrontEnd/src/app/components/inicio/register/register.component.ts
@@ -15,7 +15,7 @@ export class RegisterComponent implements OnInit {
       usuario:['',Validators.required],
       password:['',[Validators.required,Validators.minLength(4)]],
       confirmPassword:['']
-    },{Validator:this.checkPassword});
+    },{validator:this.checkPassword});
    }
 
   ngOnInit(): void {
@@ -24,9 +24,10 @@ export class RegisterComponent implements OnInit {
     console.log(this.register);
   }
   checkPassword(group :FormGroup):any{
-    const pass=group.controls.password.value;
+    /*const pass=group.controls.password.value;
     const confirmPass=group.controls.confirmPassword.value;
     return pass === confirmPass ? null : {notSame:true};
+    */
   }
 
 }
